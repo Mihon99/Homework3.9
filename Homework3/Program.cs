0ee13ab7938958184aea4ab98c44b10b5c7eb352
reaper <reaper>
2022-11-12 16:53:15 +0300

    Homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,19 @@
+﻿int M = InputValue("Введите первое число");
+int N = InputValue("Введите второе число");
+Console.WriteLine($"Функция Акермана = {AckermannFunction(M,N)} ");
+
+int InputValue(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int a = int.Parse(value);
+    return a;
+}
+
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
