3d71a7db9f6ab89bfe00a7b6d3b40c5cc0e53991
reaper <reaper>
2022-11-12 16:53:08 +0300

    Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿int M = InputValue("Введите первое число");
+int N = InputValue("Введите второе число");
+Console.WriteLine($"Сумма чисел = {SumNumbers(M,N)} ");
+
+int InputValue(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int a = int.Parse(value);
+    return a;
+}
+
+int SumNumbers (int a, int b)
+{
+    if(a > b) return 0;    
+    return a + SumNumbers(a + 1, b);
+}
+
