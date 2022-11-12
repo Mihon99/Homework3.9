d49fa4143d93d2006480b383a0f43f8d2542fc27
reaper <reaper>
2022-11-12 16:53:03 +0300

    Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿string Reqursia(int a)
+{
+    if(a == 1) return "1";
+    else return  $"{a}, {Reqursia(a - 1)}";       
+}
+
+int r = PrintAndGetValue("Максимум");
+
+Console.WriteLine($"{Reqursia(r)} ");
+
+int PrintAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    int input = int.Parse(Console.ReadLine());
+    return input;
+}
